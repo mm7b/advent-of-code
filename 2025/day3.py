6e0fd611e4863936c51d868d2b685622921f3b25
Thomas Herman <thomas.herman@famoco.com>
2025-12-03 14:54:01 +0100

    2025 - day 3 alternative solution

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 def find_batteries(banks, size):
     packs = []
     for bank in banks:
@@ -12,15 +15,31 @@ def find_batteries(banks, size):
     return sum(packs)
 
 
+def find_batteries_bis(banks, size):
+    packs = []
+    for bank in banks:
+        pack = []
+        for i in range(size):
+            max_i, max_val = max(
+                enumerate(bank[: len(bank) - (size - i - 1)]), key=itemgetter(1)
+            )
+            pack.append(max_val)
+            bank = bank[max_i + 1 :]
+        packs.append(int("".join(str(b) for b in pack)))
+    return sum(packs)
+
+
 def main():
     with open("inputs/day3.txt", "r", encoding="utf8") as f:
         banks = [[int(b) for b in line.strip()] for line in f.readlines()]
 
     # PART 1
-    print(find_batteries(banks, 2))
+    # print(find_batteries(banks, 2))
+    print(find_batteries_bis(banks, 2))
 
     # PART 2
-    print(find_batteries(banks, 12))
+    # print(find_batteries(banks, 12))
+    print(find_batteries_bis(banks, 12))
 
 
 if __name__ == "__main__":
