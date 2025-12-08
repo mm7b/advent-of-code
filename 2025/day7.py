7d6f157748cfd5d2c3b47831e04106225b3019d7
Thomas Herman <thomas.herman@famoco.com>
2025-12-08 18:28:16 +0100

    2025 - day 7 condensed

--- 2025/day7.py
+++ 2025/day7.py
@@ -1,44 +1,32 @@
 from collections import defaultdict
 
 
-def find_timelines(matrix, beams, timelines=1):
+def find_splits(matrix, beams, splits=0, unique=True):
     new_beams = defaultdict(int)
     for (r, c), nr in beams.items():
         if r == len(matrix) - 1:
             continue
         if matrix[r + 1][c]:
-            timelines += 1 * nr
-            new_beams[(r + 1, c - 1)] += nr
-            new_beams[(r + 1, c + 1)] += nr
+            splits += 1 if unique else 1 * nr
+            new_beams[(r + 1, c - 1)] += 1 if unique else nr
+            new_beams[(r + 1, c + 1)] += 1 if unique else nr
         else:
-            new_beams[(r + 1, c)] += nr
-    return find_timelines(matrix, new_beams, timelines) if new_beams else timelines
+            new_beams[(r + 1, c)] += 1 if unique else nr
+    return find_splits(matrix, new_beams, splits, unique) if new_beams else splits
 
 
 def main():
     with open("inputs/day7.txt", "r", encoding="utf8") as f:
         lines = f.readlines()
         matrix = [[1 if c == "^" else 0 for c in r.strip()] for r in lines[1:]]
+        beams = {(0, list(lines[0]).index("S")): 1}
 
     # PART 1
-    beams = {(0, list(lines[0]).index("S"))}
-    splits = 0
-    while beams:
-        new_beams = set()
-        for r, c in beams:
-            if r == len(matrix) - 1:
-                continue
-            if matrix[r + 1][c]:
-                splits += 1
-                new_beams.update([(r + 1, c - 1), (r + 1, c + 1)])
-            else:
-                new_beams.add((r + 1, c))
-        beams = new_beams
-    print(splits)
+    print(find_splits(matrix, beams))
 
     # PART 2
-    beams = {(0, list(lines[0]).index("S")): 1}
-    print(find_timelines(matrix, beams))
+    # Adding 1 "split" for the initial reality
+    print(find_splits(matrix, beams, unique=False) + 1)
 
 
 if __name__ == "__main__":
